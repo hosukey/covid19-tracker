e477e8bdf2775ab10dfef8a8ef0b9757c1ba5710
hosu <hosulee11@gmail.com>
2021-11-17 15:12:17 -0800

    select province to update numbers

--- js/app.js
+++ js/app.js
@@ -1,20 +1,73 @@
 'use strict';
 
+// --calculate percentage vaccinated = (vaccinated/popultion) * 100
+
 // variables
 const globalCases = document.querySelector('.global .cases li:nth-child(2)');
 const globalDeaths = document.querySelector('.global .deaths li:nth-child(2)');
 const globalUpdate = document.querySelector('.global__updated');
-
 const canadaCases = document.querySelector('.canada .cases li:nth-child(2)');
 const canadaDeaths = document.querySelector('.canada .deaths li:nth-child(2)');
 const canadaRecovered = document.querySelector(
   '.canada .recovered li:nth-child(2)'
 );
 const canadaUpdate = document.querySelector('.canada__updated');
-
 const provinceUpdate = document.querySelector('.province__updated');
+const provinceDataContainer = document.querySelector('.provinceDataContainer');
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+const selectProvinceBtn = document.querySelector('.prov-dropdown');
+
+let provinceData = {};
+
+// api
+const globalRequest = function () {
+  fetch('https://disease.sh/v3/covid-19/all')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('global', data);
+      timeCalculation(data);
+      getGlobalData(data);
+    });
+};
+
+const canadaRequest = function () {
+  fetch('https://disease.sh/v3/covid-19/countries/Canada?strict=true')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('canada', data);
+      timeCalculation(data);
+      getCanadaData(data);
+    });
+};
+
+const provinceRequest = function () {
+  fetch('https://api.opencovid.ca/summary')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('province', data);
+      provinceData = data;
+      getProvinceData(data);
+      selectProvinceBtn.addEventListener('change', getProvinceData);
+      dateCalculation(data);
+    });
+};
 
 // functions
+
 // calculating time updated - global & canada
 const timeCalculation = function (data) {
   const time = new Date(data.updated);
@@ -24,43 +77,14 @@ const timeCalculation = function (data) {
   const hour = time.getHours();
   const mins = time.getMinutes();
   //   console.log(year, month, day, hour, mins);
-
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
   const convertingMonth = months[month];
   return `Last updated ${convertingMonth} ${day}, ${year} ${hour}:${mins}`;
 };
 
 // calculation date updated for provinces
 const dateCalculation = function (data) {
-  const time = data.summary[0].date.split('-');
+  const time = provinceData.summary[dataSetNumber].date.split('-');
   const [day, month, year] = time;
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
   const convertingMonth = months[month - 1];
   return `Last updated ${convertingMonth} ${day}, ${year}`;
 };
@@ -83,14 +107,29 @@ const getCanadaData = function (data) {
 };
 
 const getProvinceData = function (data) {
+  const dataResult = selectProvinceBtn[selectProvinceBtn.selectedIndex];
+  const dataSetNumber = dataResult.dataset.number;
+  provinceDataContainer.textContent = '';
+
+  const dateCalculation = function (data) {
+    const time = provinceData.summary[dataSetNumber].date.split('-');
+    const [day, month, year] = time;
+    const convertingMonth = months[month - 1];
+    return `Last updated ${convertingMonth} ${day}, ${year}`;
+  };
+
   const html = `
-  <div class="province__name">${data.summary[1].province}</div>
+  <div class="province__name">${
+    provinceData.summary[dataSetNumber].province
+  }</div>
   <div class="province__items items">
     <div class="province__items--item">
       <span class="line"></span>
       <ul class="cases item">
         <li>Cases</li>
-        <li>${numberWithCommas(data.summary[1].cumulative_cases)}</li>
+        <li>${numberWithCommas(
+          provinceData.summary[dataSetNumber].cumulative_cases
+        )}</li>
       </ul>
     </div>
 
@@ -98,7 +137,9 @@ const getProvinceData = function (data) {
       <span class="line"></span>
       <ul class="deaths item">
         <li>deaths</li>
-        <li>${numberWithCommas(data.summary[1].cumulative_deaths)}</li>
+        <li>${numberWithCommas(
+          provinceData.summary[dataSetNumber].cumulative_deaths
+        )}</li>
       </ul>
     </div>
 
@@ -106,7 +147,9 @@ const getProvinceData = function (data) {
       <span class="line"></span>
       <ul class="recovered item">
         <li>recovered</li>
-        <li>${numberWithCommas(data.summary[1].cumulative_recovered)}</li>
+        <li>${numberWithCommas(
+          provinceData.summary[dataSetNumber].cumulative_recovered
+        )}</li>
       </ul>
     </div>
 
@@ -115,58 +158,17 @@ const getProvinceData = function (data) {
       <ul class="vaccinated item">
         <li>vaccinated *</li>
         <li>${numberWithCommas(
-          data.summary[1].cumulative_cvaccine
+          provinceData.summary[dataSetNumber].cumulative_cvaccine
         )}<span> (80%)</span></li>
       </ul>
     </div>
   </div>
   `;
-
-  provinceUpdate.insertAdjacentHTML('beforebegin', html);
+  provinceDataContainer.insertAdjacentHTML('beforeend', html);
   provinceUpdate.textContent = dateCalculation(data);
 };
-
-// api
-const globalRequest = function () {
-  fetch('https://disease.sh/v3/covid-19/all')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      timeCalculation(data);
-      getGlobalData(data);
-    });
-};
-
-const canadaRequest = function () {
-  fetch('https://disease.sh/v3/covid-19/countries/Canada?strict=true')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      timeCalculation(data);
-      getCanadaData(data);
-    });
-};
-
-const provinceRequest = function () {
-  fetch('https://api.opencovid.ca/summary')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      dateCalculation(data);
-      getProvinceData(data);
-    });
-};
-
-// const otherInfoRequest = function () {
-//   fetch('https://api.opencovid.ca/other?stat=prov')
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//     });
-// };
 
 // handler
 globalRequest();
 canadaRequest();
 provinceRequest();
-// otherInfoRequest();
--- practice/main.js
+++ practice/main.js
@@ -1,69 +1,97 @@
 'use strict';
 
-//https://api.opencovid.ca/summary
+// //https://api.opencovid.ca/summary
 
-const container = document.querySelector('.container');
+// const container = document.querySelector('.container');
 
-const renderCountry = function (data, className = '') {
-  const time = new Date(data.updated);
-  const year = time.getFullYear();
-  const month = time.getMonth();
-  const day = time.getDate();
-  const hour = time.getHours();
-  const mins = time.getMinutes();
-  console.log(year, month, day, hour, mins);
+// const renderCountry = function (data, className = '') {
+//   const time = new Date(data.updated);
+//   const year = time.getFullYear();
+//   const month = time.getMonth();
+//   const day = time.getDate();
+//   const hour = time.getHours();
+//   const mins = time.getMinutes();
+//   console.log(year, month, day, hour, mins);
 
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
-  const convertingMonth = months[month];
+//   const months = [
+//     'Jan',
+//     'Feb',
+//     'Mar',
+//     'Apr',
+//     'May',
+//     'Jun',
+//     'Jul',
+//     'Aug',
+//     'Sept',
+//     'Oct',
+//     'Nov',
+//     'Dec',
+//   ];
+//   const convertingMonth = months[month];
 
-  const html = `<div class="country"><span>🇨🇦</span> ${data.country}</div>
-      <div class="total-cases"><span>💣 Total cases:</span> ${data.cases}</div>
-      <div class="today-cases"><span>👀 Today's cases:</span> ${data.todayCases}</div>
-      <div class="total-deaths"><span>😵 Total deaths:</span> ${data.deaths}</div>
-      <div class="today-death"><span>☠️ Today's death:</span> ${data.todayDeaths}</div>
-      <div class="updated"><span>⏱ Updated time:</span> ${convertingMonth} ${day},  ${year} ${hour}:${mins}</div>`;
+//   const html = `<div class="country"><span>🇨🇦</span> ${data.country}</div>
+//       <div class="total-cases"><span>💣 Total cases:</span> ${data.cases}</div>
+//       <div class="today-cases"><span>👀 Today's cases:</span> ${data.todayCases}</div>
+//       <div class="total-deaths"><span>😵 Total deaths:</span> ${data.deaths}</div>
+//       <div class="today-death"><span>☠️ Today's death:</span> ${data.todayDeaths}</div>
+//       <div class="updated"><span>⏱ Updated time:</span> ${convertingMonth} ${day},  ${year} ${hour}:${mins}</div>`;
 
-  container.insertAdjacentHTML('beforeend', html);
-};
+//   container.insertAdjacentHTML('beforeend', html);
+// };
 
-const request = function () {
-  fetch('https://disease.sh/v3/covid-19/countries/Canada?strict=true')
+// const request = function () {
+//   fetch('https://disease.sh/v3/covid-19/countries/Canada?strict=true')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       renderCountry(data);
+//     });
+// };
+
+// const summary = function () {
+//   fetch('https://api.opencovid.ca/summary')
+//     .then((response1) => response1.json())
+//     .then((data1) => {
+//       console.log(data1);
+//       console.log(data1.summary[1]);
+//     });
+// };
+
+// const provinces = function () {
+//   fetch('https://api.opencovid.ca/other?stat=prov')
+//     .then((response1) => response1.json())
+//     .then((data2) => {
+//       console.log(data2);
+//     });
+// };
+// request();
+// summary();
+// provinces();
+
+const dropdown = document.querySelector('select');
+const div = document.querySelector('div');
+let provinceData = {};
+
+//api
+const provinceRequest = function () {
+  fetch('https://api.opencovid.ca/summary')
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      renderCountry(data);
+      provinceData = data;
+      html();
+      dropdown.addEventListener('change', html);
     });
 };
 
-const summary = function () {
-  fetch('https://api.opencovid.ca/summary')
-    .then((response1) => response1.json())
-    .then((data1) => {
-      console.log(data1);
-      console.log(data1.summary[1]);
-    });
-};
+//insert html
+function html(e) {
+  const dataResult = dropdown[dropdown.selectedIndex];
+  const dataSetNumber = dataResult.dataset.num;
 
-const provinces = function () {
-  fetch('https://api.opencovid.ca/other?stat=prov')
-    .then((response1) => response1.json())
-    .then((data2) => {
-      console.log(data2);
-    });
-};
-request();
-summary();
-provinces();
+  div.textContent = '';
+
+  const contents = `<p> ${dataResult.value} case: ${provinceData.summary[dataSetNumber].cumulative_cases}</p>`;
+  div.insertAdjacentHTML('beforeend', contents);
+}
+
+provinceRequest();
